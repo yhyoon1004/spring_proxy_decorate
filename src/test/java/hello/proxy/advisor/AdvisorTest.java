150de1ef8b587359d1490296ae128336d8326f33
yhyoon1004 <yhyoon1004@naver.com>
2024-06-14 08:42:12 +0900

    예제 코드1 - 어드바이저

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -13,7 +13,9 @@ public class AdvisorTest {
     void advisorTest1() {
         ServiceInterface target = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(target);
+        //어드바이저 생성, 어드바이져 =(포인트컷 + 어드바이스)
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        //어드바이저 추가
         proxyFactory.addAdvisor(advisor);
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
 
