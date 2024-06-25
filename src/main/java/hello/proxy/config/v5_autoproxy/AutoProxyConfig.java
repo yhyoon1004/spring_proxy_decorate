e1643c07f9cb3ff2bfdc055d0b8ca532c923fab3
yhyoon1004 <yhyoon1004@github.com>
2024-06-25 19:31:32 +0900

    스프링이 제공하는 빈 후처리1

---
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,27 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.AppConfigV1;
+import hello.proxy.config.AppConfigV2;
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppConfigV1.class, AppConfigV2.class})
+public class AutoProxyConfig {
+
+    @Bean
+    public Advisor getAdvisor(LogTrace logTrace) {
+        //pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*","order*", "save*");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut,advice);
+    }
+}
