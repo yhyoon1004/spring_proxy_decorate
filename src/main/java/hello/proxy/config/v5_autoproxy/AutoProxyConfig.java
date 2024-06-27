8333027b2c420c874f42cdcc9fb5faa3e9e8f824
yhyoon1004 <yhyoon1004@github.com>
2024-06-27 19:21:29 +0900

    스프링이 제공하는 빈 후처리기2

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppConfigV2;
 import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +16,8 @@ import org.springframework.context.annotation.Import;
 @Import({AppConfigV1.class, AppConfigV2.class})
 public class AutoProxyConfig {
 
-    @Bean
-    public Advisor getAdvisor(LogTrace logTrace) {
+//    @Bean     모든 객체에 아래 매핑해준 이름이 들어간 객체를 프록시 생성 문제 발생
+    public Advisor advisor1(LogTrace logTrace) {
         //pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*","order*", "save*");
@@ -24,4 +25,26 @@ public class AutoProxyConfig {
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut,advice);
     }
+
+//    @Bean
+//    public Advisor advisor2(LogTrace logTrace) {
+//        //pointcut
+//        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+//        pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+//                            // * -> 모든 반환타입 , hello.~ -> 포인트컷을 적용할 경로    ||(..)-> 모든 파라미터 상관없음
+//        //advice
+//        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+//        return new DefaultPointcutAdvisor(pointcut,advice);
+//    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        //pointcut
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");//제외 조건 추가
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut,advice);
+    }
+
 }
